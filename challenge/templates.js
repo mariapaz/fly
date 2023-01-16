3397f9fdc40a6d9446744d8b397a48155985abeb
Maria Paz Garcia <maria.paz.garcia.silva@itv.com>
2023-01-16 12:45:56 +0000

    sanitize

--- challenge/templates.js
+++ challenge/templates.js
@@ -1,16 +1,26 @@
-function home(posts) {
+function home(posts, errors = {}, values = {}) {
   const title = "All posts";
   const content = /*html*/ `
     <h2>New post</h2>
     <form method="POST">
       <p>
         <label for="nickname">Nickname</label>
-        <input id="nickname" name="nickname">
+        <input
+          id="nickname"
+          name="nickname"
+          value="${values.nickname ? sanitize(values.nickname) : ""}"
+        >
+        ${validation(errors.nickname)}
       </p>
       <p>
         <label for="message">Message</label>
-        <textarea id="message" name="message"></textarea>
-      </p>
+        <textarea
+          id="message"
+          name="message">
+            ${values.message ? sanitize(values.message) : ""}
+          </textarea>
+        ${validation(errors.message)}
+        </p>
       <button>Send</button>
     </form>
     <h2>All posts</h2>
@@ -21,13 +31,25 @@ function home(posts) {
   return layout(title, content);
 }
 
+function sanitize(unsafe) {
+  return unsafe.replace(/</g, "&lt;");
+}
+
+function validation(message) {
+  if (message) {
+    return `<span style="color: red">${message}</span>`;
+  } else {
+    return "";
+  }
+}
+
 function postItem(post) {
   const date = new Date(post.created);
   const prettyDate = date.toLocaleString("en-GB");
   return `
     <li>
-      <p>${post.message}</p>
-      <p>—${post.nickname} | ${prettyDate}</p>
+      <p>${sanitize(post.message)}</p>
+      <p>—${sanitize(post.nickname)} | ${prettyDate}</p>
     </li>
   `;
 }
